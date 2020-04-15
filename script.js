e9aef95b9389d3c5d17caba3f6c542629dc53464
Yakraj Pariyar <59973173+yakraj@users.noreply.github.com>
2020-04-15 23:50:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+//ternary js is a simple language as you learn. 
+//here i'll teach you first how we should start.
+
+var name = "yakraj";
+var reply = name == "yakraj" ? "hello" : "i don't know you man";
+
+//same way if i'll code with if conditional syntax
+var name = "yakarj";
+function reply(){
+if (name = "yakraj"){
+return "hello";
+}
+else{
+return "i don't know you man";
+}
+}
+
+//same all the way we can make some boolians
+function iwonGame(bool){
+  return bool;
+}
+var whatDidyou = iwonGame(false) ?"Really i Won this Game" : "decent work";
+var whatDidyou = iwonGame(true) ?"Really i Won this Game" : "decent work";
+
+//ternary is the very easy way if you want to make if else condition 
+//but there is another syntax if you want like if, else, if else, so you can use antoher syntax named switch this is also really useful 
+//whether you can check there on my github account https://github.com/yakraj/
